c01dfa0d38c24d967f375849779050c94511bafb
minhbaodlld <minhbaodlld@users.noreply.github.com>
2021-05-11 17:09:59 +0700

    C# example project

---
+++ C#/simpleRPG/Engine/LivingCreature.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Engine
+{
+    public class LivingCreature : INotifyPropertyChanged
+    {
+        private int _currentHitPoints { get; set; }
+        public int CurrentHitPoints
+        {
+            get { return _currentHitPoints; }
+            set
+            {
+                _currentHitPoints = value;
+                OnPropertyChanged("CurrentHitPoints");
+            }
+        }
+        public int MaximumHitPoints { get; set; }
+
+        public LivingCreature(int currentHitPoints, int maximumHitPoints)
+        {
+            CurrentHitPoints = currentHitPoints;
+            MaximumHitPoints = maximumHitPoints;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected void OnPropertyChanged(string name)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
